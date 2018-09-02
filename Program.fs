b68f325533cb70dba4715971618934e6c22e5065
Petr Chromec <mortal_flesh@seznam.cz>
2018-09-03 01:14:14 +0200

    Add list command

--- Program.fs
+++ Program.fs
@@ -6,7 +6,14 @@ open ImageManager.PrepareForSorting
 
 type Command =
 | Invalid
+| List
 | PrepareForSorting of PrepareForSorting
+
+let listCommands () =
+    ["prepare";"list"]
+    |> List.map (sprintf "- %s")
+    |> String.concat "\n"
+    |> sprintf "Available commands:\n%s"
 
 [<EntryPoint>]
 let main argv =
@@ -17,12 +24,14 @@ let main argv =
         match argv with
         | [|"prepare";source;target|] -> PrepareForSorting { source = source; target = target; exclude = None }
         | [|"prepare";source;target;exclude|] -> PrepareForSorting { source = source; target = target; exclude = Some exclude }
+        | [|"list"|] -> List
         | _ -> Invalid
 
     let (message, result) =
         match command with
-        | Invalid -> ("Unknown command", 1)
+        | List -> (listCommands(), 0)
         | PrepareForSorting p -> prepareForSorting p
+        | Invalid -> ("Unknown command", 1)
 
     printfn "%s" message
     result
--- README.md
+++ README.md
@@ -5,6 +5,17 @@ Simple console app for managing images.
 
 ## Commands
 
+### List
+Will show a list of available commands.
+```sh
+dotnet run list
+```
+```
+Available commands:
+- prepare
+- list
+```
+
 ### Prepare for sorting
 Prepares images from source dir (_recursively_) into the target dir, it might exclude images from a specific dir (_recursively_).
 
